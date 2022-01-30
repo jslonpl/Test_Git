14f991ddb7094b4c435f341ae0bfb717fb800eca
jslonpl <jslonpl@gmail.com>
2022-01-30 15:35:39 +0100

    Initial Commit

---
+++ Test_Git/Test_GitApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_GitApp.swift
+//  Test_Git
+//
+//  Created by Jakub Słoń on 30/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct Test_GitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
